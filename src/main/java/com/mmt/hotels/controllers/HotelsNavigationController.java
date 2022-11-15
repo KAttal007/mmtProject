f676cb5bbe7052fe6bda4f2ca4ece51221ba660e
KAttal007 <kaushalattal03@gmail.com>
2022-11-15 14:05:18 +0530

    some change idk

--- src/main/java/com/mmt/hotels/controllers/HotelsNavigationController.java
+++ src/main/java/com/mmt/hotels/controllers/HotelsNavigationController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mmt.flights.controllers.BookFlightController;
 import com.mmt.hotels.model.Hotel;
 
 @Controller
